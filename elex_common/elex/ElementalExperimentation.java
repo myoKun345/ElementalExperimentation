f0b8a3138ffc812aaae4b7b5a7685e342a26f26c
Myo-Kun <myo.kun345@gmail.com>
2013-06-09 19:46:04 -0400

    First classes

---
+++ elex_common/elex/ElementalExperimentation.java
@@ -0,0 +1,28 @@
+package elex;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import elex.lib.Reference;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class ElementalExperimentation {
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
---
+++ elex_common/elex/lib/Reference.java
@@ -0,0 +1,8 @@
+package elex.lib;
+
+public class Reference {
+    /* Generic Mod Constants */
+    public static final String MOD_ID = "ElEx";
+    public static final String MOD_NAME = "Elemental Experimentation";
+    public static final String VERSION = "0.0.01.001-dev";
+}
