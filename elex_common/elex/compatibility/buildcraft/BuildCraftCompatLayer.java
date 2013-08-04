897619f8c8b3abb2061741bc59a81d30ba7182bd
Myo-Kun <myo.kun345@gmail.com>
2013-08-04 14:20:41 -0400

    Git y u be derpy

--- elex_common/elex/compatibility/buildcraft/BuildCraftCompatLayer.java
+++ elex_common/elex/compatibility/buildcraft/BuildCraftCompatLayer.java
@@ -1,17 +1,5 @@
 package elex.compatibility.buildcraft;
 
-import java.util.List;
-
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import elex.ElementalExperimentation;
-import elex.lib.ItemIds;
-import elex.lib.Reference;
-import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Icon;
 
 /**
  * Elemental Experimentation
@@ -22,43 +10,5 @@ import net.minecraft.util.Icon;
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
 public class BuildCraftCompatLayer {
-
-    @SideOnly(Side.CLIENT)
-    private Icon[] icons;
-    
-    public ItemMachinePart(int par1) {
-        super(par1);
-        setCreativeTab(ElementalExperimentation.elexTab);
-        setHasSubtypes(true);
-    }
-    
-    @Override
-    public String getUnlocalizedName(ItemStack stack) {
-        return "item." + ItemIds.MACHINE_PART_REAL_UNLOCALIZED_NAMES[stack.getItemDamage()];
-    }
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister register) {
-        icons = new Icon[ItemIds.MACHINE_PART_REAL_UNLOCALIZED_NAMES.length];
-        for (int i = 0; i < icons.length; i++) {
-            icons[i] = register.registerIcon(Reference.MOD_ID + ":" + ItemIds.MACHINE_PART_REAL_UNLOCALIZED_NAMES[i]);
-        }
-    }
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    public Icon getIconFromDamage(int dmg) {
-        return icons[dmg];
-    }
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void getSubItems(int id, CreativeTabs tab, List list) {
-        for (int i = 0; i < ItemIds.MACHINE_PART_REAL_UNLOCALIZED_NAMES.length; i++) {
-            ItemStack stack = new ItemStack(id, 1, i);
-            list.add(stack);
-        }
-    }
 
 }
