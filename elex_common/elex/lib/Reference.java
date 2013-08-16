b023153ecfc44c0281679c4bd9d6f5f63228287a
Myo-kun <myo.kun345@gmail.com>
2013-08-16 14:33:50 -0400

    Maybe Jenkins will do the build number automatically now???

--- elex_common/elex/lib/Reference.java
+++ elex_common/elex/lib/Reference.java
@@ -12,7 +12,7 @@ public class Reference {
     /* Generic Mod Constants */
     public static final String MOD_ID = "ElEx";
     public static final String MOD_NAME = "Elemental Experimentation";
-    public static final String VERSION = "0.0.01.001";
+    public static final String VERSION = "0.0.01.$BUILD_NUMBER";
     public static final String CHANNEL = "elex";
     public static final int SHIFTED_ID_RANGE_CORRECTION = 256;
 }
